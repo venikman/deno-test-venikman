ae05860b016e204496284fce3cbf0136e12ee741
venikman <nedbailov375426@gmail.com>
2019-12-29 16:56:51 -0500

    Fixes usage of env 4

---
+++ main.js
@@ -0,0 +1,14 @@
+
+import { serve } from "https://deno.land/std@v0.27.0/http/server.ts";
+import * as flags from "https://deno.land/std@v0.27.0/flags/mod.ts";
+
+import pogo from 'https://deno.land/x/pogo/main.js';
+import * as flags from "https://deno.land/std@v0.27.0/flags/mod.ts";
+const defaultPort = 3000;
+const argPort = flags.parse(Deno.args).port;
+const port = argPort ? Number(argPort) : defaultPort;
+const server = pogo.server({ port });
+server.router.get('/', () => {
+    return 'Hello, world!';
+});
+server.start();
