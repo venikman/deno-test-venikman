d6e4578862b5aff47b8b6ba303f7d7a3e07688b9
venikman <nedbailov375426@gmail.com>
2020-01-01 20:40:10 -0500

    Add JSX

--- main.jsx
+++ main.jsx
@@ -1,10 +1,15 @@
+import React from 'https://dev.jspm.io/react';
 import pogo from 'https://deno.land/x/pogo/main.js';
 import * as flags from "https://deno.land/std@v0.27.0/flags/mod.ts";
+
+
 const defaultPort = 3000;
 const argPort = flags.parse(Deno.args).port;
 const port = argPort ? Number(argPort) : defaultPort;
-const server = pogo.server({ port, hostname : '0.0.0.0' });
+const server = pogo.server({ port });
+
 server.router.get('/', () => {
-    return 'Hello, world!';
+    return  <h1>Hello from JSX!</h1>;
 });
+
 await server.start();
